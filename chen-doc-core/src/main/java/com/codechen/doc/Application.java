12315f120042b3ed44251c0815ef693e3da71acd
chenyl8848 <1063415880@qq.com>
2024-06-24 16:21:23 +0800

    init:项目初始化

---
+++ chen-doc-core/src/main/java/com/codechen/doc/Application.java
@@ -0,0 +1,17 @@
+package com.codechen.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：Java陈序员
+ * @date：2024/6/24 16:13
+ * @description：主启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+}
